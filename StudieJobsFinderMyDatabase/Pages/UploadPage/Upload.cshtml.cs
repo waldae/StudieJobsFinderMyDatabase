5f99d172c103fda1e01183cf8d93dfdb2c9fe07c
Jou0001 <jout0001@edu.zealand.dk>
2024-05-21 23:11:59 +0200

    Opdatering Ny version, af det jeg har sat ind

---
+++ StudieJobsFinderMyDatabase/Pages/UploadPage/Upload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudieJobsFinderMyDatabase.Pages.UploadPage
+{
+    public class UploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
